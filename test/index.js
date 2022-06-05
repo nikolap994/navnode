68d2f6225a42a0b25e6a6fd3f589de5c89bbab02
Nikola Pavlović <nikolap994@gmail.com>
2022-06-05 20:04:42 +0200

    Add local env for testing and test commands

--- test/index.js
+++ test/index.js
@@ -2,11 +2,11 @@ const deployer = require("../index");
 
 deployer.initConfig({
   default: {
-    deployTo: "/var/apps/super-project",
+    deployTo: "/home/nikola_server/test",
     repositoryUrl: "https://github.com/user/super-project.git",
   },
   staging: {
-    servers: "deploy@staging.super-project.com",
+    servers: "nikola_server@192.168.56.101",
   },
 });
 
