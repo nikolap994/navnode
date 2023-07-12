e2796799cd7c57d327eeca543d60a4df7fe7d8d6
Marko Ranđelović <marko@stuntcoders.com>
2023-07-12 12:19:28 +0200

    Update messaging displayed for different tasks

--- bin/index.js
+++ bin/index.js
@@ -11,13 +11,20 @@ env = arguments[0].split(':')[0];
 task = arguments[0].split(':')[1];
 
 if (env && task) {
-  console.log(colors.blue("Navnode deploying to " + env + ':' + task));
+  if (task.includes("deploy")) {
+    console.log(colors.blue("Navnode deploying to " + env));
+  } else if (!task.includes("rsync")) {
+    console.log(colors.blue("Navnode executing " + env + ':' + task));
+  }
+
   if ('rsync_pull' === task) {
+    console.log(colors.blue("Navnode syncing files from " + env));
     navnode.rsyncPull(env, arguments[1]);
     return;
   }
 
   if ('rsync_push' === task) {
+    console.log(colors.blue("Navnode syncing files to " + env));
     navnode.rsyncPush(env, arguments[1]);
     return;
   }
