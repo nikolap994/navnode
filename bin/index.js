e14e03fd1728ce170f1d1e0192f55598491ad2db
Nikola Pavlović Ph.D <nikolap994@gmail.com>
2023-09-15 21:37:44 +0200

    Improve code style

--- bin/index.js
+++ bin/index.js
@@ -6,37 +6,29 @@ const navnode = require("../lib/navnode");
 /**
  * The main entry point for the Navnode command-line utility.
  */
-var arguments = process.argv.splice(2);
-var env = null;
-var task = null;
-
-// Parse the provided arguments to determine the environment and task.
-env = arguments[0].split(':')[0];
-task = arguments[0].split(':')[1];
+const arguments = process.argv.slice(2);
+const arg = arguments[0];
+const [env, task] = arg.split(':');
 
 if (env && task) {
   if (task.includes("deploy")) {
-    console.log(colors.blue("Navnode deploying to " + env));
+    console.log(colors.blue(`Navnode deploying to ${env}`));
   } else if (!task.includes("rsync")) {
-    console.log(colors.blue("Navnode executing " + env + ':' + task));
+    console.log(colors.blue(`Navnode executing ${env}:${task}`));
   }
 
-  if ('rsync_pull' === task) {
-    console.log(colors.blue("Navnode syncing files from " + env));
+  if (task === 'rsync_pull') {
+    console.log(colors.blue(`Navnode syncing files from ${env}`));
     navnode.rsyncPull(env, arguments[1]);
-    return;
-  }
-
-  if ('rsync_push' === task) {
-    console.log(colors.blue("Navnode syncing files to " + env));
+  } else if (task === 'rsync_push') {
+    console.log(colors.blue(`Navnode syncing files to ${env}`));
     navnode.rsyncPush(env, arguments[1]);
-    return;
-  }
+  } else {
+    const deployment = navnode.deploy(env, task);
 
-  const deployment = navnode.deploy(env, task);
-  
-  if (typeof deployment !== "undefined") {
-    console.log(colors.blue(navnode.deploy(env)));
+    if (typeof deployment !== "undefined") {
+      console.log(colors.blue(deployment));
+    }
   }
 } else {
   console.log(colors.red("Missing environment or task"));
