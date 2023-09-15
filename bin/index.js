34dbc19c90d5bfc4ef7edcfdce3f637614dec9a4
Nikola Pavlović Ph.D <nikolap994@gmail.com>
2023-09-16 00:55:49 +0200

    Add init command

--- bin/index.js
+++ bin/index.js
@@ -2,13 +2,14 @@
 
 const colors = require("colors");
 const navnode = require("../lib/navnode");
+const path = require("path");
 
 /**
  * The main entry point for the Navnode command-line utility.
  */
 const arguments = process.argv.slice(2);
 const arg = arguments[0];
-const [env, task] = arg.split(':');
+const [env, task] = arg.split(":");
 
 if (env && task) {
   if (task.includes("deploy")) {
@@ -17,10 +18,10 @@ if (env && task) {
     console.log(colors.blue(`Navnode executing ${env}:${task}`));
   }
 
-  if (task === 'rsync_pull') {
+  if (task === "rsync_pull") {
     console.log(colors.blue(`Navnode syncing files from ${env}`));
     navnode.rsyncPull(env, arguments[1]);
-  } else if (task === 'rsync_push') {
+  } else if (task === "rsync_push") {
     console.log(colors.blue(`Navnode syncing files to ${env}`));
     navnode.rsyncPush(env, arguments[1]);
   } else {
@@ -31,5 +32,22 @@ if (env && task) {
     }
   }
 } else {
-  console.log(colors.red("Missing environment or task"));
+  if (arg === "init") {
+    const configFile = "navnode-deployment.js";
+    try {
+      const config = require(path.resolve(`./${configFile}`));
+      if (config) {
+        console.log(`${configFile} already found.`);
+      }
+      return config;
+    } catch (err) {
+      if (err.code === "MODULE_NOT_FOUND") {
+        console.log(`${configFile} not found. Creating the file...`);
+        navnode.init();
+        console.log(`${configFile} created successfully. Please configure it.`);
+      }
+    }
+  } else {
+    console.log(colors.red("Missing environment or task"));
+  }
 }
